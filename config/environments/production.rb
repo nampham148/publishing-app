63407ae7d2756618fef90505ea441179792ade5d
nampham148 <phamquangnam148@gmail.com>
2018-06-20 09:06:41 +0700

    add mailer functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,18 @@ Rails.application.configure do
   #force ssl
   config.force_ssl = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'nam-publishing-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
 end
