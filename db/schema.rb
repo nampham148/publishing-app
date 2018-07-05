55b16c18ad7f274254d64c7022a6741dd58e0aae
nampham148 <phamquangnam148@gmail.com>
2018-07-05 08:40:52 +0700

    add number of friends for facebook users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627021812) do
+ActiveRecord::Schema.define(version: 20180704032503) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180627021812) do
     t.datetime "updated_at",                                             null: false
     t.text     "image",                  default: "default-profile.jpg"
     t.boolean  "admin",                  default: false
+    t.string   "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
